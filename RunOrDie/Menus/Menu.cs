57b9180d9335625653102c02092d6b7e5dd86390
Tor Bjurbom <34156187+Bjurbom@users.noreply.github.com>
2018-05-22 12:32:07 +0200

    more stuff on the way

---
+++ RunOrDie/Menus/Menu.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunOrDie.Menus
+{
+    class Menu
+    {
+        protected int choices;
+        protected int Selection;
+        protected Rectangle[] rectangles;
+
+        public Menu(int choices)
+        {
+            this.choices = choices;
+        }
+
+        
+    }
+}
---
+++ RunOrDie/Menus/PauseMenu/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoGame
+
+namespace RunOrDie.Menus.PauseMenu
+{
+    class PauseMenu : Menu
+    {
+        public PauseMenu(int choices):base(choices)
+        {
+
+        }
+
+
+    }
+}
